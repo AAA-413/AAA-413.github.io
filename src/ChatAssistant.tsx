d9a2ace57b2c720a3063898e7c4dc147f2336449
yanghe <1339260251@qq.com>
2026-05-11 17:01:26 +0800

    refine assistant mascot

--- src/ChatAssistant.tsx
+++ src/ChatAssistant.tsx
@@ -151,6 +151,7 @@ function RobotMascot() {
       <div className="robot-head">
         <span className="robot-eye left" />
         <span className="robot-eye right" />
+        <span className="robot-mouth" />
       </div>
       <div className="robot-body">
         <Bot size={22} />
